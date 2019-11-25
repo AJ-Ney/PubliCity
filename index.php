59a9fb303d150c41d8a0e139f21a46d666738d35
AJ-Ney <57320585+AJ-Ney@users.noreply.github.com>
2019-11-24 20:00:17 -0500

    Maquetado del Modal V1

--- index.php
+++ index.php
@@ -40,14 +40,36 @@
     
     <aside id="modal" class="modal">
         <div class="content-modal">
-            <header>
+            <header class="header-modal">
                 <a href="#" class="close-modal">X</a>
-                <h2>Titulo del modal</h2>
+                <h2>PubliCity</h2>
             </header>
+            <hr>
             <article>
                 <!-- contenido -->
+                <div class="cont-text">
+                    <h2>Titulo 1</h2>
+                    <h4>Subtituli Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla omnis unde, voluptate, nisi beatae dolores veritatis iusto earum ducimus recusandae ulla</h4>
+                </div>
+                <div class="contenidonoticia">
+                    <img src="https://i.ytimg.com/vi/n4RMu7CZ5ow/maxresdefault.jpg" alt="Noticia">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis illo officia eveniet. Velit aliquid natus excepturi. Deserunt aut itaque neque! Provident cum odio maxime molestiae suscipit expedita quod natus exercitationem!
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias beatae aliquam sequi ex? Facilis voluptates vero blanditiis voluptatem? Reiciendis incidunt maiores eos labore sit at dolores quaerat tempora ducimus rerum?
+                    Delectus, quibusdam. Laudantium dolores cumque corrupti animi quo error sequi, unde excepturi optio molestiae eligendi deserunt corporis quis hic, reprehenderit, eveniet pariatur eaque ut perspiciatis vel voluptas! Aperiam, vitae tempore?
+                    Ratione quo architecto atque ab officia blanditiis earum dolore aspernatur veniam deserunt itaque eveniet non est hic dolorem id, quidem magnam debitis incidunt? Sit neque obcaecati beatae velit accusantium perspiciatis!
+                </div>
             </article>
+            <hr>
             <footer>
+                <p class="redactor">
+                &nbsp;&nbsp;&nbsp;&nbsp;Redactor 1
+                </p>
+                <p class="publicado">
+                    Publicado el: 24/11/2019
+                    &nbsp;&nbsp;
+                    <span><i>Leidas:</i></span>
+                    &nbsp;&nbsp;&nbsp;&nbsp;
+                </p>
                 <!-- final del modal -->
             </footer>
         </div>
